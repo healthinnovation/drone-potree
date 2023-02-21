67c1efeed86cfbcb30970c9a8d7052ef1fba543d
ambarja <bryan.fernandez.c@upch.pe>
2023-02-21 09:43:18 -0500

    updated

---
+++ santa-maria/pointclouds/index.laz/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 1202178,
+    "boundingBox": {
+        "lx": 308012.43,
+        "ly": 8625668.85,
+        "lz": 113.13,
+        "ux": 308412.21,
+        "uy": 8626068.629999999,
+        "uz": 512.9100000000279
+    },
+    "tightBoundingBox": {
+        "lx": 308012.43,
+        "ly": 8625668.85,
+        "lz": 113.13,
+        "ux": 308412.21,
+        "uy": 8626031.51,
+        "uz": 212.24
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 3.4621963500976564,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
